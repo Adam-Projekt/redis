b42ec0a177fd105ee0db7d9783de580b073686d8
Adam MegaRules <adammegarules1@gmail.com>
2026-04-18 11:33:32 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -4,9 +4,24 @@ console.log("Logs from your program will appear here!");
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
-  connection.on("data", () => {
-    connection.write("+PONG\r\n");
+  connection.on("data", (data: Buffer) => {
+    const stringifiedData = data.toString();
+    const arrayData = stringifiedData.split("\r\n");
+    console.log("arrayData", arrayData);
+    const command = arrayData[2].toLocaleLowerCase();
+    console.log("command", command);
+
+    switch (command) {
+      case "echo":
+        const responseData = `${arrayData[3]}\r\n${arrayData[4]}\r\n`;
+        connection.write(responseData);
+        break;
+
+      default:
+        connection.write("+PONG\r\n");
+        break;
+    }
+    // connection.write("+PONG\r\n");
   });
 });
-
 server.listen(6379, "127.0.0.1");
