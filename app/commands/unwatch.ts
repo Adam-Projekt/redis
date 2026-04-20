6c7655c36155bccfe51e185428ed553de10e05a6
Adam <adammegarules1@proton.me>
2026-04-20 19:11:02 +0200

    codecrafters submit [skip ci]

--- app/commands/unwatch.ts
+++ app/commands/unwatch.ts
@@ -2,10 +2,6 @@ import type { Client } from "../class";
 import { BulkError, SimpleString } from "../helper";
 
 export function unwatch(arg: string[], client: Client) {
-  if (arg.length < 1) {
-    return BulkError("ERR must use at least 1 parameter");
-  }
-
   client.clearWatch();
   return SimpleString("OK");
 }
