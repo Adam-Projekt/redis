cee8976a575842094aa02e437365bb0a9c7babbe
Adam <adammegarules1@proton.me>
2026-04-20 11:32:20 +0200

    codecrafters submit [skip ci]

--- app/command.ts
+++ app/command.ts
@@ -203,7 +203,7 @@ export async function handle(arg: string[], command: Commands, client: Client) {
       let response2: string[] = [];
       if (arg.length > 1) {
         vari = Number(arg[1]);
-        for (let i = 0; i <= vari; i++) {
+        for (let i = 0; i < vari; i++) {
           response2.push(arra.data[i]);
         }
       }
