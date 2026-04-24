48af99c6aa73b5b60c093535db76bdc6a4c41ef6
Adam <adammegarules1@proton.me>
2026-04-24 12:14:29 +0200

    codecrafters submit [skip ci]

--- app/commands/subscribe/unsubscribe.ts
+++ app/commands/subscribe/unsubscribe.ts
@@ -2,26 +2,22 @@ import type { Client } from "../../class";
 import { ErrorMessages } from "../../error";
 import { BulkArray, BulkInteger, BulkString } from "../../helper";
 
-
-
 export function unsubscribe(arg: string[], client: Client) {
-    if (arg.length != 1) {
-      return BulkString(ErrorMessages.WRONG_ARG_COUNT("subscribe", 1));
-    }
-    const key = arg[0];
-    if (!client.subscribeTo.includes(key)) {
-      const index = client.subscribeTo.indexOf(key);
-      client.subscribeTo.splice(index,1)
-    }
-    client.subscribeMode = true;
-
-    return BulkArray(
-      [
-        BulkString("unsubscribe"),
-        BulkString(key),
-        BulkInteger(client.subscribeTo.length),
-      ],
-      false,
-    );
+  if (arg.length != 1) {
+    return BulkString(ErrorMessages.WRONG_ARG_COUNT("unsubscribe", 1));
   }
+  const key = arg[0];
+  if (!client.subscribeTo.includes(key)) {
+    const index = client.subscribeTo.indexOf(key);
+    client.subscribeTo.splice(index, 1);
+  }
+
+  return BulkArray(
+    [
+      BulkString("unsubscribe"),
+      BulkString(key),
+      BulkInteger(client.subscribeTo.length),
+    ],
+    false,
+  );
 }
