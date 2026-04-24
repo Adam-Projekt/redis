c40654b7096424322027cd6a4541ea1844f30bba
Adam <adammegarules1@proton.me>
2026-04-24 08:03:45 +0200

    codecrafters submit [skip ci]

--- app/commands/subscripe/sub.ts
+++ app/commands/subscripe/sub.ts
@@ -1,10 +1,13 @@
 import { ErrorMessages } from "../../error";
-import { BulkArray, BulkString } from "../../helper";
+import { BulkArray, BulkInteger, BulkString } from "../../helper";
 
 export function subscribe(arg: string[]) {
   if (arg.length != 1) {
     return BulkString(ErrorMessages.WRONG_ARG_COUNT("subscribe", 1));
   }
   const key = arg[0];
-  return BulkArray([BulkString("subscribe"), BulkString(key)], false);
+  return BulkArray(
+    [BulkString("subscribe"), BulkString(key), BulkInteger(1)],
+    false,
+  );
 }
